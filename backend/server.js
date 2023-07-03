b22ffd8864d45e4849e29bb718a562bfc30e4515
Nency129 <nencypatel129@gmail.com>
2023-07-03 21:19:05 +0530

    new

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,19 @@
 const express = require("express");
-const dotenv =require("dotenv");
+const dotenv = require("dotenv");
+const connectDB = require("./config/db");
 
 
-const app=express();
+dotenv.config();
+connectDB();
+const app = express();
+
 
 app.get('/',(req,res)=>{
     res.send("Api is working");
 })
 
 const PORT=process.env.PORT || 5000;
-
-app.listen(PORT,console.log("server started"));
+// console.log(PORT);
+app.listen(PORT,()=>{
+    console.log("server started");
+});
